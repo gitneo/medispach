57292d9d6005de4cfd99cb0f72afe8b0947def0b
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-07-23 23:29:23 +0100

    chore: Setup spring boot project with init dependencies

---
+++ src/main/java/com/musala/medispach/MedispachApplication.java
@@ -0,0 +1,13 @@
+package com.musala.medispach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedispachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedispachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musala/medispach/MedispachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.medispach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedispachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
