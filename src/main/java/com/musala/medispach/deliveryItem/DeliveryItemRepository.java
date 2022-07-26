0abe2c19b20c4fa0c3f70df9e1e86a81a6da36c2
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-07-26 13:43:06 +0100

    feat: Create delivery item repository

---
+++ src/main/java/com/musala/medispach/deliveryItem/DeliveryItemRepository.java
@@ -0,0 +1,6 @@
+package com.musala.medispach.deliveryItem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeliveryItemRepository extends CrudRepository<DeliveryItem,Long> {
+}
