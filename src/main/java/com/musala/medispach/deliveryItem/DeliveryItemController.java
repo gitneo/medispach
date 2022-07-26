c70ad259db42fb562c8e424d0b795706c2757941
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-07-26 13:47:10 +0100

    feat: Create delivery item controller

---
+++ src/main/java/com/musala/medispach/deliveryItem/DeliveryItemController.java
@@ -0,0 +1,56 @@
+package com.musala.medispach.deliveryItem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medispatch/api/v1")
+public class DeliveryItemController {
+
+    private final DeliveryItemService deliveryItemService;
+
+
+    @Autowired
+    public DeliveryItemController(DeliveryItemService deliveryItemService){
+        this.deliveryItemService = deliveryItemService;
+    }
+
+
+    @PostMapping("/deliveryItem")
+    public ResponseEntity<DeliveryItemDto> createDeliveryItem(@RequestBody DeliveryItemDto deliveryItemDto){
+        DeliveryItemDto createdDeliveryItem =  this.deliveryItemService.createDelivery(deliveryItemDto);
+        return new ResponseEntity<>(createdDeliveryItem, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/deliveryItem/{id}")
+    public ResponseEntity<DeliveryItemDto> getDeliveryItem(@PathVariable("id") long id){
+        DeliveryItemDto deliveryItem = this.deliveryItemService.getDeliveryItem(id);
+        return new ResponseEntity<>(deliveryItem,HttpStatus.OK);
+    }
+
+
+    @GetMapping("/deliveryItems")
+    public ResponseEntity<List<DeliveryItemDto>> getDeliveryItems(){
+        List<DeliveryItemDto> deliveryItems = this.deliveryItemService.getDeliveryItems();
+        return new ResponseEntity<>(deliveryItems,HttpStatus.OK);
+    }
+
+
+    @PutMapping("/deliveryItem")
+    public ResponseEntity<DeliveryItemDto> updatedDeliveryItem(@RequestBody DeliveryItemDto deliveryItemDto){
+        DeliveryItemDto updatedDeliveryItem = this.deliveryItemService.updateDelivery(deliveryItemDto);
+        return new ResponseEntity<>(updatedDeliveryItem,HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/deliveryItem/{id}")
+    public ResponseEntity<String> deleteDeliverItem(@PathVariable long id){
+        this.deliveryItemService.deleteDeliveryItem(id);
+        return new ResponseEntity<>("The delivery item with id "+id+" has been deleted", HttpStatus.OK);
+    }
+}
