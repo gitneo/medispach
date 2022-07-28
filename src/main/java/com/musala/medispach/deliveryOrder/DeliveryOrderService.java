97960d5bc2458682185a4571d3af55477c5a58ef
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-07-28 02:18:54 +0100

    feat: create delivery order service layer

---
+++ src/main/java/com/musala/medispach/deliveryOrder/DeliveryOrderService.java
@@ -0,0 +1,59 @@
+package com.musala.medispach.deliveryOrder;
+
+import com.musala.medispach.deliveryItem.DeliveryItemService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.NoResultException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DeliveryOrderService {
+
+    private final DeliveryOrderRepository deliveryOrderRepository;
+
+
+    public DeliveryOrderService(DeliveryOrderRepository deliveryOrderRepository){
+        this.deliveryOrderRepository = deliveryOrderRepository;
+    }
+
+
+    public DeliveryOrderDto createDeliveryOrder(DeliveryOrderDto deliveryOrderDto){
+        DeliveryOrder deliveryOrder = DeliveryOrderMapper.instance.toDeliveryOrder(deliveryOrderDto);
+        DeliveryOrder createdDeliveryOrder = this.deliveryOrderRepository.save(deliveryOrder);
+
+        return DeliveryOrderMapper.instance.toDto(createdDeliveryOrder);
+    }
+
+
+    public DeliveryOrderDto getDeliveryOrder(long id){
+        DeliveryOrder deliveryOrder = this.deliveryOrderRepository.findById(id).orElseThrow(NoResultException::new);
+        return DeliveryOrderMapper.instance.toDto(deliveryOrder);
+    }
+
+
+    public List<DeliveryOrderDto> getDeliveryOrders(){
+        List<DeliveryOrderDto> deliveryOrderDtoList =  new ArrayList<>();
+
+        this.deliveryOrderRepository.findAll().forEach(deliveryOrder -> {
+            deliveryOrderDtoList.add(DeliveryOrderMapper.instance.toDto(deliveryOrder));
+        });
+
+        return deliveryOrderDtoList;
+    }
+
+
+    public DeliveryOrderDto updateDeliveryOrder(DeliveryOrderDto deliveryOrderDto){
+        DeliveryOrder deliveryOrder = DeliveryOrderMapper.instance.toDeliveryOrder(deliveryOrderDto);
+        DeliveryOrder updatedDeliveryOrder = this.deliveryOrderRepository.save(deliveryOrder);
+
+        return DeliveryOrderMapper.instance.toDto(updatedDeliveryOrder);
+    }
+
+
+    public void deleteDeliveryOrder(long id){
+        this.deliveryOrderRepository.deleteById(id);
+    }
+
+}
