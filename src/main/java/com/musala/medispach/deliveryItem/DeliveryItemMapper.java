2a306626ec49cbe0b78ef68cba08c1c20bf2ce8b
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-08-02 01:39:12 +0100

    Update delivery item mapper
    *map objects to fields and vice versa

--- src/main/java/com/musala/medispach/deliveryItem/DeliveryItemMapper.java
+++ src/main/java/com/musala/medispach/deliveryItem/DeliveryItemMapper.java
@@ -1,6 +1,10 @@
 package com.musala.medispach.deliveryItem;
 
+import com.musala.medispach.deliveryItem.entity.DeliveryItem;
+import com.musala.medispach.deliveryOrder.DeliveryOrder;
+import com.musala.medispach.medication.Medication;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -8,6 +12,36 @@ public interface DeliveryItemMapper {
 
     DeliveryItemMapper instance = Mappers.getMapper(DeliveryItemMapper.class);
 
+    @Mapping(target = "medicationId", source = "medication")
+    @Mapping(target = "deliverOrderId", source = "deliverOrder")
     DeliveryItemDto toDto(DeliveryItem deliveryItem);
+
+    @Mapping(target = "medication", source = "medicationId")
+    @Mapping(target = "deliverOrder", source = "deliverOrderId")
     DeliveryItem toDeliveryItem(DeliveryItemDto deliveryItemDto);
+
+
+    default long getMedicationId(Medication medication){
+        return medication.getId();
+    }
+
+    default Medication getMedication(long id){
+        Medication medication =  new Medication();
+        medication.setId(id);
+
+        return medication;
+    }
+
+
+    default long getDeliverOrderId( DeliveryOrder deliveryOrder){
+        return deliveryOrder.getId();
+    }
+
+
+    default DeliveryOrder getDeliveryOrder( long id){
+        DeliveryOrder deliveryOrder = new DeliveryOrder();
+        deliveryOrder.setId(id);
+
+        return deliveryOrder;
+    }
 }
