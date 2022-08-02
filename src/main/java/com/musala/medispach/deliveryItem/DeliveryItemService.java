681723dffec55077d37f0e91124deef91f74ae22
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-08-02 01:41:31 +0100

    Update delivery item service return types

--- src/main/java/com/musala/medispach/deliveryItem/DeliveryItemService.java
+++ src/main/java/com/musala/medispach/deliveryItem/DeliveryItemService.java
@@ -1,6 +1,7 @@
 package com.musala.medispach.deliveryItem;
 
-import com.musala.medispach.delivery.DeliveryDto;
+import com.musala.medispach.deliveryItem.entity.DeliveryItem;
+import com.musala.medispach.deliveryItem.entity.DeliveryItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
