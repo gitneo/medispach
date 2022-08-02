56827f43f7ecc6afe17c26c166da4f5e9476c61f
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-08-02 01:20:23 +0100

    Add validation to drone DTO

--- src/main/java/com/musala/medispach/drone/DroneDto.java
+++ src/main/java/com/musala/medispach/drone/DroneDto.java
@@ -1,9 +1,9 @@
 package com.musala.medispach.drone;
 
-import com.musala.medispach.delivery.Delivery;
+import com.musala.medispach.delivery.entity.Delivery;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -13,9 +13,11 @@ import java.util.List;
 public class DroneDto {
     private long id;
 
-    @NotNull private int serialNo;
+    @NotNull
+    @Max(value = 100,message = "Maximum number of characters(100) exceeded")
+    private String serialNo;
     @NotNull private DroneModel model;
-    @NotNull private BigDecimal weightLimit;
+    @NotNull final private BigDecimal weightLimit = new BigDecimal("500");
     @NotNull private BigDecimal batteryCapacity;
     @NotNull private DroneState state;
     @NotNull private List<Delivery> deliveryList =  new ArrayList<>();
