3a90eb9fe68954516b4e71f6b0f31d849aa3d755
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-08-04 06:57:51 +0100

    Create JPA query projection to fix failing query * add drone list item projection

---
+++ src/main/java/com/musala/medispach/DroneListItems.java
@@ -0,0 +1,11 @@
+package com.musala.medispach;
+
+import java.math.BigDecimal;
+
+public interface DroneListItems {
+    long getMed_id();
+    String getMed_code();
+    String getMed_image();
+    String getMed_name();
+    BigDecimal getMed_weight();
+}
