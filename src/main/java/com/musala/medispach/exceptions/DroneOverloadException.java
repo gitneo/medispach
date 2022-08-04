5c234942d0a9f991f6e783fc4de3b8c03ad6a2b3
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-08-04 05:04:14 +0100

    create custom Drone Overload exception

---
+++ src/main/java/com/musala/medispach/exceptions/DroneOverloadException.java
@@ -0,0 +1,7 @@
+package com.musala.medispach.exceptions;
+
+public class DroneOverloadException extends Exception{
+    public DroneOverloadException(String message) {
+        super(message);
+    }
+}
