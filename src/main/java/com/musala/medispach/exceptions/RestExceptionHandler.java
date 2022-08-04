266b2cc9e15ab7c38ed87389d9e63b4054e44b47
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-08-04 05:02:54 +0100

    Handle API exceptions
    * handle NoResultException
    * handle EmptyResultDataAccessException
    * handle MethodArgumentNotValid

--- src/main/java/com/musala/medispach/exceptions/RestExceptionHandler.java
+++ src/main/java/com/musala/medispach/exceptions/RestExceptionHandler.java
@@ -2,16 +2,24 @@ package com.musala.medispach.exceptions;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.NoResultException;
 import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -19,29 +27,60 @@ import java.util.NoSuchElementException;
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<ErrorResponse> handleSQLIntegrityException(HttpServletRequest req, SQLIntegrityConstraintViolationException ex){
+    public ResponseEntity<ErrorResponse> handleSQLIntegrityException(HttpServletRequest req, SQLIntegrityConstraintViolationException ex) {
         String errorMessage = "Unable to submit post: " + ex.getMessage();
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, errorMessage);
 
-        return new ResponseEntity<>(errorResponse,errorResponse.getHttpStatus());
+        return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
     }
 
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ErrorResponse> handleNoSuchElementException(HttpServletRequest request, NoSuchElementException ex){
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(HttpServletRequest request, NoSuchElementException ex) {
         String errorMessage = "The row for the address is not existent " + request.getRequestURI();
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND,errorMessage);
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, errorMessage);
 
-        return new ResponseEntity<>(errorResponse,errorResponse.getHttpStatus());
+        return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+    }
+
+
+
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleEmptyResultDataAccessException(HttpServletRequest httpServletRequest, EmptyResultDataAccessException ex) {
+        String message = ex.getMessage();
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, message);
+
+        return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
     }
 
 
     @ExceptionHandler(NoResultException.class)
-    public  ResponseEntity<ErrorResponse> handleNoResultException(HttpServletRequest httpServletRequest, NoResultException ex) {
-        String message = "The items requested for is not available";
-        ErrorResponse errorResponse =  new ErrorResponse(HttpStatus.NOT_FOUND,message);
+    public ResponseEntity<ErrorResponse> handleNoResultException(HttpServletRequest httpServletRequest, NoResultException ex) {
+        String message = "The request yielded no result";
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, message);
 
-        return new ResponseEntity<>(errorResponse,errorResponse.getHttpStatus());
+        return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
     }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+            List<String> errors = new ArrayList<String>();
+            for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+                errors.add(error.getField() + ": " + error.getDefaultMessage());
+            }
+            for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+                errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+            }
+
+
+             ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, errors.get(0));
+            return handleExceptionInternal(
+                    ex, errorResponse, headers, errorResponse.getHttpStatus(), request);
+    }
+
+
 
 }
